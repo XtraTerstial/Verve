7079029223e72c9c7a8d489ba395cffdae92cd57
Devbrat Pradhan <devbratpradhan40@gmail.com>
2025-12-18 23:29:39 +0530

    Initial commit

---
+++ src/main/java/com/XtraCoder/Verve/VerveApplication.java
@@ -0,0 +1,13 @@
+package com.XtraCoder.Verve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerveApplication.class, args);
+	}
+
+}
