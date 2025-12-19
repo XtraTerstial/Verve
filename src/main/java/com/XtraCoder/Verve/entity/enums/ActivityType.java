a84db64095070790617c858aa6d9f126be159c04
Devbrat Pradhan <devbratpradhan40@gmail.com>
2025-12-19 20:16:45 +0530

    v1.0

---
+++ src/main/java/com/XtraCoder/Verve/entity/enums/ActivityType.java
@@ -0,0 +1,14 @@
+package com.XtraCoder.Verve.entity.enums;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
